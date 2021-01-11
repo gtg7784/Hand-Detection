766b2216207b921dc61c14ec0e642562a5524be0
gtg7784 <tae.gun7784@gmail.com>
2021-01-12 01:36:27 +0900

    Create Xcode Project with Swift & SwiftUI

---
+++ HandDetection/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HandDetection
+//
+//  Created by 고태건 on 2021/01/12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ HandDetection/HandDetectionApp.swift
@@ -0,0 +1,17 @@
+//
+//  HandDetectionApp.swift
+//  HandDetection
+//
+//  Created by 고태건 on 2021/01/12.
+//
+
+import SwiftUI
+
+@main
+struct HandDetectionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
